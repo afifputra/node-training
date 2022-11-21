ca902566b0d73bf09c96bdf5f0b3566739705b97
afifputra <afifputraharyono@gmail.com>
2022-11-21 16:26:29 +0700

    try to send response and how to threat request

--- 1-basic-concept/app.ts
+++ 1-basic-concept/app.ts
@@ -3,8 +3,32 @@ import { IncomingMessage, ServerResponse } from "http";
 const http = require("http");
 
 const rqListener = (req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.url, req.method, req.headers);
-  res.end("Hello World");
+  const { url, method } = req;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/message" && method === "POST") {
+    const body: any[] = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  // res.setHeader("Content-Type", "application/json");
+  // res.write(JSON.stringify({ message: "Hello World" }));
+  // res.end();
 };
 
 const server = http.createServer(rqListener);
