240fa360d2ace2daec410d98e8501d7558f8fc6e
afifputra <afifputraharyono@gmail.com>
2022-11-22 10:17:22 +0700

    save to a file

--- 1-basic-concept/app.ts
+++ 1-basic-concept/app.ts
@@ -12,23 +12,33 @@ const rqListener = (req: IncomingMessage, res: ServerResponse) => {
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write("</html>");
     return res.end();
-  } else if (url === "/message" && method === "POST") {
-    const body: any[] = [];
+  }
 
-    req.on("data", (chunk: any) => {
-      body.push(chunk);
+  if (url === "/message" && method === "POST") {
+    const body: string[] = [];
+
+    req.on("data", (chunk: Buffer) => {
+      const parsedChunk = chunk.toString();
+      body.push(parsedChunk);
     });
 
     req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
+      const parsedBody = body.join("");
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
     });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 
-  // res.setHeader("Content-Type", "application/json");
-  // res.write(JSON.stringify({ message: "Hello World" }));
-  // res.end();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 };
 
 const server = http.createServer(rqListener);
