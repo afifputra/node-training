852d672e9ef96b7cb5067785975690eefcdfbee1
afifputra <afifputraharyono@gmail.com>
2022-11-23 15:37:49 +0700

    add middleware

--- 2-working-with-express/basic/src/app.ts
+++ 2-working-with-express/basic/src/app.ts
@@ -1,6 +1,17 @@
-const http = require("http");
-const routes = require("./routes");
+import express from "express";
+import http from "http";
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another the middleware!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3001);
