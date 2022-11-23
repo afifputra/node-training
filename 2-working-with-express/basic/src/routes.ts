2633d6c5b580cbd29aad3c1a73f0a83415c8070c
afifputra <afifputraharyono@gmail.com>
2022-11-23 15:16:19 +0700

    setup for express project

---
+++ 2-working-with-express/basic/src/routes.ts
@@ -0,0 +1,42 @@
+import { IncomingMessage, ServerResponse } from "http";
+const fs = require("fs");
+
+const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
+  const { url, method } = req;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Try Soo Soo</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body: string[] = [];
+
+    req.on("data", (chunk: Buffer) => {
+      const parsedChunk = chunk.toString();
+      body.push(parsedChunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = body.join("");
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("./dist/message.txt", message, (err: Error) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+module.exports = requestHandler;
