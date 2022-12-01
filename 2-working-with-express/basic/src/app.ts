a3fedb386ed9093848a092212d73b7932a317fdf
afifputra <afifputraharyono@gmail.com>
2022-12-01 16:11:41 +0700

    parse incoming data

--- 2-working-with-express/basic/src/app.ts
+++ 2-working-with-express/basic/src/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In another the middleware!");
-  res.send('<h1>The "Add Product" Page!</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another the middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 
