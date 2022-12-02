f532fab0b1446fee2351f14d661fa9c5e1338ac1
afifputra <afifputraharyono@gmail.com>
2022-12-02 20:34:29 +0700

    serving html file

--- 2-working-with-express/basic/routes/admin.ts
+++ 2-working-with-express/basic/routes/admin.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import path from "path";
 
 const router = Router();
 
 // GET => /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, "../", "../", "views", "add-product.html"));
 });
 
 // POST => /admin/add-product
