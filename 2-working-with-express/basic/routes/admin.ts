4862ed7a93e90d785a8e5ff4c40bf551dc910c61
afifputra <afifputraharyono@gmail.com>
2022-12-02 20:06:14 +0700

    spliting routes

---
+++ 2-working-with-express/basic/routes/admin.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+module.exports = router;
--- 2-working-with-express/basic/src/app.ts
+++ 2-working-with-express/basic/src/app.ts
@@ -2,20 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+const adminRoutes = require("../routes/admin");
+const shopRoutes = require("../routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body.title);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3001);
