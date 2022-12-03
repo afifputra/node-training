843ea099b6ea741d97cbf0153a8c690533272c9a
afifputra <afifputraharyono@gmail.com>
2022-12-03 20:18:28 +0700

    serving static file

--- 2-working-with-express/basic/app.ts
+++ 2-working-with-express/basic/app.ts
@@ -3,10 +3,14 @@ import bodyParser from "body-parser";
 import path from "path";
 
 const app = express();
+
+const rootDir = require("./utils/path");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "../", "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
