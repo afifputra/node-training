3d8d831c6cf39d66e4ae44ed8c99b25a2080fb6a
afifputra <afifputraharyono@gmail.com>
2022-12-03 19:59:06 +0700

    add concurently and helper functions

--- 2-working-with-express/basic/routes/admin.ts
+++ 2-working-with-express/basic/routes/admin.ts
@@ -3,9 +3,11 @@ import path from "path";
 
 const router = Router();
 
+const rootDir = require("../utils/path");
+
 // GET => /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // POST => /admin/add-product
