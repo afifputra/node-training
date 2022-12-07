db180d9568cdd6c410782f1faeba95eae93b47ce
afifputra <afifputraharyono@gmail.com>
2022-12-07 16:10:59 +0700

    try using ejs

--- 3-templating-engines/basic/app.ts
+++ 3-templating-engines/basic/app.ts
@@ -9,17 +9,7 @@ const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const expressHbs = require("express-handlebars");
-
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
