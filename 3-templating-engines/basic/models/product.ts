ad1da812e70630a7cc873e088362423a0726262f
afifputra <afifputraharyono@gmail.com>
2022-12-08 11:41:52 +0700

    saving data to file

--- 3-templating-engines/basic/models/product.ts
+++ 3-templating-engines/basic/models/product.ts
@@ -1,19 +1,42 @@
-const products: {
+import fs from "fs";
+import path from "path";
+
+type Product = {
   title: string;
-}[] = [];
+};
+
+const pathRoot = path.join(path.dirname(require.main?.filename!), "../", "data", "products.json");
 
 module.exports = class Product {
   title: string;
 
-  constructor(title: string) {
-    this.title = title;
+  constructor(inputTitle: string) {
+    this.title = inputTitle;
   }
 
   save() {
-    products.push(this);
+    fs.readFile(pathRoot, (err, fileContent) => {
+      let savedProducts: Product[] = [];
+
+      if (!err) {
+        savedProducts = JSON.parse(fileContent.toString());
+      }
+
+      savedProducts.push(this);
+
+      fs.writeFile(pathRoot, JSON.stringify(savedProducts), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(pathRoot, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      const products: Product[] = JSON.parse(fileContent.toString());
+      return products;
+    });
   }
 };
