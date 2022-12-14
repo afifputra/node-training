f17c58dab9d27d8501d19d770022b6d45b426fcc
afifputra <afifputraharyono@gmail.com>
2022-12-14 09:42:58 +0700

    add user models

---
+++ 3-templating-engines/basic/models/user.ts
@@ -0,0 +1,18 @@
+import Sequelize from "sequelize";
+import sequelize from "../utils/database";
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+export default User;
