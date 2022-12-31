4b14f45c2ca6eadf688957dc323e3f88f3a230df
afifputra <afifputraharyono@gmail.com>
2022-12-31 11:21:10 +0700

    make product schema

--- 3-templating-engines/basic/models/product.ts
+++ 3-templating-engines/basic/models/product.ts
@@ -1,3 +1,26 @@
+import { Schema } from "mongoose";
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+export default productSchema;
+
 // import { ObjectId } from "mongodb";
 // import { getDb } from "../utils/database";
 
