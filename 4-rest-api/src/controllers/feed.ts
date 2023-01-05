0bb77d3cc3e7ce394d6ce187f6d9edcd9e577f3e
afifputra <afifputraharyono@gmail.com>
2023-01-05 16:12:14 +0700

    update

--- 4-rest-api/src/controllers/feed.ts
+++ 4-rest-api/src/controllers/feed.ts
@@ -1,6 +1,21 @@
 import { RequestHandler } from "express";
 
-const getPosts: RequestHandler = (_, res, __) => {};
+const getPosts: RequestHandler = (_, res, __) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Maximilian",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
 
 export default {
   getPosts,
