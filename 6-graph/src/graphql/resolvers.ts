743cf9d844e4889f6543ca4bd17104426c53b24d
afifputra <afifputraharyono@gmail.com>
2023-01-17 14:38:35 +0700

    try to use mutation and using grphiql for debugging

--- 6-graph/src/graphql/resolvers.ts
+++ 6-graph/src/graphql/resolvers.ts
@@ -1,12 +1,43 @@
-const hello = () => {
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+
+const createUser = async (
+  {
+    userInput,
+  }: {
+    userInput: {
+      email: string;
+      password: string;
+      name: string;
+    };
+  },
+  __: any
+) => {
+  const { email, name, password } = userInput;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new Error("User exists already.");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = new User({
+    email,
+    name,
+    password: hashedPassword,
+  });
+
+  const createdUser = (await user.save()).toJSON();
+
   return {
-    text: "Hello World!",
-    views: 1234,
+    ...createdUser,
   };
 };
 
 const resolvers = {
-  hello,
+  createUser,
 };
 
 export default resolvers;
--- 6-graph/src/graphql/schema.ts
+++ 6-graph/src/graphql/schema.ts
@@ -1,17 +1,42 @@
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
 
-    type Query {
-        hello: TestData!
+    type User {
+        _id: ID!
+        email: String!
+        password: String
+        name: String!
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInputData {
+        email: String!
+        password: String!
+        name: String!
+    }
+
+    type RootQuery {
+        hello: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
     }
 
     schema {
-        query: Query
+        query: RootQuery
+        mutation: RootMutation
     }
 `);
 
