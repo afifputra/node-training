58c8ced5681d8dcb3362bd838b8ad80eed315b7f
afifputra <afifputraharyono@gmail.com>
2023-01-17 14:45:57 +0700

    try to throw error

--- 6-graph/src/graphql/resolvers.ts
+++ 6-graph/src/graphql/resolvers.ts
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import bcrypt from "bcryptjs";
+import validator from "validator";
 
 const createUser = async (
   {
@@ -13,7 +14,25 @@ const createUser = async (
   },
   __: any
 ) => {
+  const errors: { message: string }[] = [];
   const { email, name, password } = userInput;
+
+  if (!validator.isEmail(email)) {
+    errors.push({
+      message: "E-Mail is invalid.",
+    });
+  }
+
+  if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+    errors.push({
+      message: "Password too short!",
+    });
+  }
+
+  if (errors.length > 0) {
+    const error = new Error("Invalid input.");
+    throw error;
+  }
 
   const existingUser = await User.findOne({ email });
 
